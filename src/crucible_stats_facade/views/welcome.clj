c8a01aecd45b7c84a1477573969a8367981d7a5d
Janne Rintanen <janne.rintanen@gmail.com>
2012-10-20 18:10:00 +0300

    removed useless common and welcome

--- src/crucible_stats_facade/views/welcome.clj
+++
@@ -1,9 +0,0 @@
-(ns crucible_stats_facade.views.welcome
-  (:require [crucible_stats_facade.views.common :as common]
-            [cheshire.core :as json]
-            [noir.content.getting-started])
-  (:use [noir.core :only [defpage]]))
-
-(defpage "/welcome" []
-         (common/layout
-           [:p "Welcome to crucible_stats_facade xx2"]))
