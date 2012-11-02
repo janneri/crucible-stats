e109a0eba344cf45179ded1c7ab5873d9a4b8714
Janne Rintanen <janne.rintanen@gmail.com>
2012-11-02 10:22:59 +0200

    katselmoinnin korjauksia

--- src/crucible_stats_facade/crucible_client.clj
+++ src/crucible_stats_facade/crucible_client.clj
@@ -3,7 +3,7 @@
             [clojure.xml :as xml]
             [clj-http.client :as client]))
 
-(def token (atom {}))
+(defonce token (atom {}))
 
 (defn token-param []
   (if-let [tokenStr (:tokenStr @token)]
@@ -12,11 +12,11 @@
 
 (def valid-review-states "Draft,Approval,Review,Summarize,Closed")
 
-(def base-uri "http://localhost:8080/rest-service/")
+(def base-uri "http://localhost:8090/rest-service/")
 (defn login-uri [username password] (str base-uri "auth-v1/login?userName=" username "&password=" password))
-(def projects-uri (str base-uri "projects-v1?" token-param))
-(def reviews-uri (str base-uri "reviews-v1?" token-param "&state=" valid-review-states))
-(defn comments-uri [review-id] (str base-uri "reviews-v1/" review-id "/comments?" token-param))
+(defn projects-uri [] (str base-uri "projects-v1?" (token-param)))
+(defn reviews-uri [] (str base-uri "reviews-v1?" (token-param) "&state=" valid-review-states))
+(defn comments-uri [review-id] (str base-uri "reviews-v1/" review-id "/comments?" (token-param)))
 
 
 (defn login [username password]
@@ -31,17 +31,17 @@
     (swap! token assoc :tokenStr new-token))) 
         
 (defn project-ids []
-  (let [json-data (json/parse-string (:body (client/get projects-uri)) true)
+  (let [json-data (json/parse-string (:body (client/get (projects-uri))) true)
         projects (:projects json-data)]
     (map (fn [pdata] (get-in pdata [:projectData :key])) projects)))
 
 
 (defn comments [review-id]
-  (let [json-data (json/parse-string (:body (client/get (comments-uri review-id))) true)
+  (let [json-data (json/parse-string (:body (client/get ((comments-uri) review-id))) true)
         comments (:comments json-data)]
     comments))
 
 (defn reviews []
-  (let [json-data (json/parse-string (:body (client/get reviews-uri)) true)
+  (let [json-data (json/parse-string (:body (client/get (reviews-uri))) true)
         reviews (:reviews json-data)]
     reviews))
--- src/crucible_stats_facade/views/stats_facade.clj
+++ src/crucible_stats_facade/views/stats_facade.clj
@@ -12,7 +12,7 @@
    :review-count (count (:reviews @cached-data))
    :comments-updated (:comments-updated @cached-data)})
 
-(declare comment-stats-for-all)
+(declare comment-stats-for-all get-review-ids)
 
 (defn update-cached-comments [reviews]
   (swap! cached-data assoc :comments-updated (now) 
@@ -66,12 +66,17 @@
   (map review-date review-vector))
 
 (defn group-reviews-by-month [review-vector]
-  (map (fn [[[y m] s]] {:year y :month m :count (count s)}) 
-    (group-by (juxt :year :month) (review-dates review-vector))))
+  (for [[[year month] dates] (group-by (juxt :year :month)
+                                       (review-dates review-vector))]
+    {:year year
+     :month month
+     :count (count dates)}))
 
 (defn group-reviews-by-author [review-vector]
-  (let [authors (map (fn [review] (get-in review [:reviewData :author :userName])) review-vector)]
-        (map (fn [pair] {:author (first pair) :count (second pair)}) (frequencies authors))))
+  (for [[author count] (->> review-vector
+                         (map (comp :userName :author :reviewData))
+                         frequencies)]
+    {:author author, :count count}))
 
 (defn included-by-project? [excluded-project-keys review]
   (not-in? excluded-project-keys (get-in review [:reviewData :projectKey])))
@@ -86,14 +91,6 @@
     (> 0 (compare since-str (to-str (review-date review))))
     true))
 
-(defn combine-filters [filters review] 
-  (every? #(= true %) (map #(% review) filters)))
-
-(defn create-filters [excluded-projects-str since-str review]
-  (combine-filters [(partial since-filter since-str) 
-                    (partial project-key-filter excluded-projects-str)] 
-    review))
-
 
 ;(defpage "/update-cache" {:keys [sinceDate]}
 ;  (json/encode 
@@ -101,5 +98,6 @@
 (defpage "/reviews-per-month" {:keys [excludedProjects sinceDate]}
   (json/encode 
     (group-reviews-by-month 
-      (filter #(create-filters excludedProjects sinceDate %) (get-reviews)))))
-
+      (filter (every-pred (partial since-filter sinceDate)
+                          (partial project-key-filter excludedProjects))
+              (get-reviews)))))
